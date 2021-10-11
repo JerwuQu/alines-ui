f70ed12b3dcb978a031ddf2c3413e0c452979023
Marcus Ramse <marcus@ramse.se>
2021-10-11 17:07:47 +0200

    Init

---
+++ lib/alines_connection.dart
@@ -0,0 +1,185 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:async/async.dart';
+
+class ConnectionInfo {
+  String address, password;
+  int port;
+  ConnectionInfo(this.address, this.port, this.password);
+}
+
+class OutgoingPacket {
+  List<int> data = List.empty(growable: true);
+
+  OutgoingPacket();
+
+  addU8(int n) {
+    data.add(n);
+  }
+
+  addU16(int n) {
+    data.add((n / 256).floor());
+    data.add(n % 256);
+  }
+
+  addString(String str) {
+    List<int> bytes = utf8.encode(str);
+    addU16(bytes.length);
+    data.addAll(bytes);
+  }
+
+  send(Socket sock) {
+    sock.add(data);
+    sock.flush();
+  }
+}
+
+class Menu {
+  final String title;
+  final List<String> entries;
+  late bool multipleChoice, customEntry;
+  Menu(this.title, this.entries, int flags) {
+    multipleChoice = (flags & 1) > 0;
+    customEntry = (flags & 2) > 0;
+  }
+}
+
+class AlinesEvent {}
+
+class ConnectedEvent implements AlinesEvent {}
+
+class OpenMenuEvent implements AlinesEvent {
+  final Menu menu;
+  OpenMenuEvent(this.menu);
+}
+
+class CloseMenuEvent implements AlinesEvent {}
+
+class DisconnectEvent implements AlinesEvent {
+  final String message;
+  DisconnectEvent(this.message);
+}
+
+class DestroyedEvent implements AlinesEvent {}
+
+class AlinesConnection {
+  final ConnectionInfo info;
+  final StreamController<AlinesEvent> _eventStream = StreamController();
+  final StreamController<int> _dataStream = StreamController<int>();
+  late StreamQueue<int> _byteQueue;
+
+  bool _connected = false;
+  bool _destroyed = false;
+  late Socket _socket;
+
+  AlinesConnection(this.info) {
+    Socket.connect(info.address, info.port).then((socket) {
+      if (_destroyed) {
+        socket.destroy();
+        return;
+      }
+
+      _socket = socket;
+      socket.listen((data) {
+        for (var el in data) {
+          _dataStream.add(el);
+        }
+      }).onError((err) {
+        destroy();
+      });
+
+      _byteQueue = StreamQueue<int>(_dataStream.stream);
+      _readPacketsEvents().listen((event) => _eventStream.add(event));
+      _eventStream.add(ConnectedEvent());
+
+      _connected = true;
+
+      var connReq = OutgoingPacket();
+      connReq.addString(info.password);
+      connReq.send(socket);
+    }).catchError((err) {
+      destroy();
+    });
+  }
+
+  Future<int> _readU8() async => _byteQueue.next;
+
+  Future<int> _readU16() async => (await _readU8()) * 256 + (await _readU8());
+
+  Future<String> _readString() async {
+    var len = await _readU16();
+    return utf8.decode(await _byteQueue.take(len));
+  }
+
+  Stream<AlinesEvent> eventStream() => _eventStream.stream;
+
+  Stream<AlinesEvent> _readPacketsEvents() async* {
+    while (true) {
+      try {
+        var packetId = await _readU8();
+        if (packetId == 0) {
+          yield DisconnectEvent(await _readString());
+          destroy();
+        } else if (packetId == 1) {
+          var flags = await _readU8();
+          var entryCount = await _readU16();
+          var title = await _readString();
+          var entries = <String>[];
+          for (var i = 0; i < entryCount; i++) {
+            entries.add(await _readString());
+          }
+          yield OpenMenuEvent(Menu(title, entries, flags));
+        } else {
+          throw "Invalid incoming packet id";
+        }
+      } catch (err) {
+        break;
+      }
+    }
+  }
+
+  void destroy() {
+    if (_destroyed) {
+      return;
+    }
+    _destroyed = true;
+    if (_connected) {
+      _socket.destroy();
+      _dataStream.close();
+      _connected = false;
+    }
+    _eventStream.add(DestroyedEvent());
+    _eventStream.close();
+  }
+
+  void closeMenu() {
+    var packet = OutgoingPacket();
+    packet.addU8(0);
+    packet.send(_socket);
+  }
+
+  void selectSingleEntry(int id) {
+    var packet = OutgoingPacket();
+    packet.addU8(1);
+    packet.addU16(id);
+    packet.send(_socket);
+  }
+
+  void selectMultipleEntries(List<int> ids) {
+    var packet = OutgoingPacket();
+    packet.addU8(2);
+    packet.addU16(ids.length);
+    for (var id in ids) {
+      packet.addU16(id);
+    }
+    packet.send(_socket);
+  }
+
+  void customEntry(String entry) {
+    var packet = OutgoingPacket();
+    packet.addU8(3);
+    packet.addString(entry);
+    packet.send(_socket);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'dart:async';
+
+import 'package:alines/alines_connection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const AlinesApp());
+}
+
+class AlinesApp extends StatelessWidget {
+  const AlinesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'alines',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SettingsPage(),
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('Loading...'));
+  }
+}
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('Menu!'));
+  }
+}
+
+class ConnectionPage extends StatefulWidget {
+  final ConnectionInfo connInfo;
+
+  const ConnectionPage({required this.connInfo, Key? key}) : super(key: key);
+
+  @override
+  State<ConnectionPage> createState() => ConnectionPageState();
+}
+
+class ConnectionPageState extends State<ConnectionPage> {
+  late AlinesConnection connection;
+  bool connected = false;
+  Menu? menu;
+
+  ConnectionPageState() : super();
+
+  @override
+  void initState() {
+    super.initState();
+    connection = AlinesConnection(widget.connInfo);
+
+    late StreamSubscription<AlinesEvent> sub;
+    sub = connection.eventStream().listen((e) {
+      if (e is ConnectedEvent) {
+        setState(() => connected = true);
+      } else if (e is OpenMenuEvent) {
+        setState(() => menu = e.menu);
+      } else if (e is CloseMenuEvent) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Host closed menu')));
+        setState(() => menu = null);
+      } else if (e is DisconnectEvent) {
+        sub.cancel();
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Disconnected: ' + e.message)));
+        Navigator.pop(context);
+      } else if (e is DestroyedEvent) {
+        sub.cancel();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text(connected ? 'Connection lost' : 'Failed to connect')));
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    late Widget w;
+    if (!connected) {
+      w = Scaffold(
+        appBar: AppBar(title: const Text('Connecting...')),
+        body: const Center(child: CircularProgressIndicator()),
+      );
+    } else if (menu != null) {
+      w = Scaffold(
+          appBar: AppBar(
+              title:
+                  Text('${menu!.title} (${menu!.entries.length.toString()})')),
+          body: ListView.builder(
+            itemCount: menu!.entries.length,
+            itemBuilder: (context, index) => ListTile(
+              title: Text(menu!.entries[index]),
+              onTap: () {
+                connection.selectSingleEntry(index);
+                setState(() => menu = null);
+              },
+            ),
+          ));
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Waiting for menu...')),
+        body: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    return WillPopScope(
+        onWillPop: () async {
+          if (menu == null) {
+            connection.destroy();
+            return true;
+          } else {
+            connection.closeMenu();
+            setState(() => menu = null);
+            return false;
+          }
+        },
+        child: w);
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => SettingsPageState();
+}
+
+class SettingsPageState extends State<SettingsPage> {
+  late ConnectionInfo connInfo;
+  late Future<void> _loadPrefs;
+
+  SettingsPageState() : super() {
+    _loadPrefs = loadPrefs();
+  }
+
+  Future<void> savePrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('conn-addr', connInfo.address);
+    prefs.setInt('conn-port', connInfo.port);
+    prefs.setString('conn-pass', connInfo.password);
+  }
+
+  Future<void> loadPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    connInfo = ConnectionInfo(
+      prefs.getString('conn-addr') ?? '192.168.x.x',
+      prefs.getInt('conn-port') ?? 64937,
+      prefs.getString('conn-pass') ?? '',
+    );
+  }
+
+  Future<void> connect() async {
+    await savePrefs();
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => ConnectionPage(connInfo: connInfo)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+        future: _loadPrefs,
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          }
+          return Scaffold(
+              appBar: AppBar(title: const Text('Connection Settings')),
+              body: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Center(
+                    child: Column(children: [
+                  TextFormField(
+                      decoration: const InputDecoration(hintText: 'Address'),
+                      initialValue: connInfo.address,
+                      onChanged: (str) => {connInfo.address = str}),
+                  TextFormField(
+                      decoration: const InputDecoration(hintText: 'Port'),
+                      keyboardType: TextInputType.number,
+                      initialValue: connInfo.port.toString(),
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      onChanged: (str) =>
+                          {connInfo.port = int.tryParse(str) ?? 0}),
+                  TextFormField(
+                      decoration: const InputDecoration(hintText: 'Password'),
+                      initialValue: connInfo.password,
+                      onChanged: (str) => {connInfo.password = str}),
+                  TextButton(
+                      onPressed: () => {connect()},
+                      style: TextButton.styleFrom(
+                        primary: Theme.of(context).colorScheme.primary,
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        elevation: 3,
+                        padding: const EdgeInsets.all(8),
+                      ),
+                      child: const Text('Connect')),
+                ])),
+              ));
+        });
+  }
+}
